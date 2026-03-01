4f3dba7e8c0ce8c16637cadf19d621ec0992c72c
Varrahan <varrahan@gmail.com>
2026-02-28 21:46:32 -0500

    Add Outbox  entity

---
+++ src/main/java/com/p2p/payment/domain/entity/OutboxEvent.java
@@ -0,0 +1,39 @@
+package com.p2p.payment.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "outbox_events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OutboxEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "event_type", nullable = false)
+    private String eventType;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(nullable = false, columnDefinition = "jsonb")
+    private String payload;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean published = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+}
