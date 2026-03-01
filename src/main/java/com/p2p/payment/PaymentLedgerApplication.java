f33a98b169440907fe245e141edaa65ccf9164f8
Varrahan <varrahan@gmail.com>
2026-03-01 02:07:00 -0500

    Update payment ledger app

--- src/main/java/com/p2p/payment/PaymentLedgerApplication.java
+++ src/main/java/com/p2p/payment/PaymentLedgerApplication.java
@@ -2,10 +2,12 @@ package com.p2p.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ConfigurationPropertiesScan
 public class PaymentLedgerApplication {
 
     public static void main(String[] args) {
